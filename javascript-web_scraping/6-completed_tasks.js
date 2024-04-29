1a80d9a11042e32b7c19d5814ca1bd13206cae8c
rivero04 <7710@holbertonstudents.com>
2024-04-29 09:50:53 -0700

    Task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+function getCompletedTasks() {
+    request(url, { json: true }, (error, response, body) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        const completedTasks = {};
+        body.forEach(task => {
+            if (task.completed) {
+                if (!completedTasks[task.userId]) {
+                    completedTasks[task.userId] = 0;
+                }
+                completedTasks[task.userId]++;
+            }
+        });
+        console.log(completedTasks);
+    });
+}
+
+getCompletedTasks();
