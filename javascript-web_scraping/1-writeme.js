fd10d8508f191f0f10c25b9cf3319c2bebf72b52
rivero04 <7710@holbertonstudents.com>
2024-04-29 07:29:28 -0700

    Task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const data = process.argv[3];
+
+function Writefile(path, content) {
+    fs.writeFile(path, content, 'utf-8', (err) => {
+       if (err) {
+         console.error(err);
+       } else {
+         console.log('The string has been written to the file successfully.')
+       }
+    });
+   }
+   
+Writefile(filePath, data);
