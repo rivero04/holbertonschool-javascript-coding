7013c722d20a128f07e8f6a94faee5a2bd6c3c8a
rivero04 <7710@holbertonstudents.com>
2024-04-29 08:04:07 -0700

    Task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = "https://swapi-api.hbtn.io/api/films/" + movieId + "/";
+
+
+function getMovieTitle(url) {
+ request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+ });
+}
+getMovieTitle(url);
