0fac704546962506e8c46931171867f797f741fa
rivero04 <7710@holbertonstudents.com>
2024-04-29 09:26:44 -0700

    Task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+function getContent(url, filepath){
+    request(url, { encoding: 'utf8' }, (error, response, body) => {
+        if (error){
+            console.log(error)
+            return;
+        }
+        else{
+            fs.writeFileSync(filepath, body, 'utf-8');
+        }
+    });
+}
+getContent(url,filePath);
