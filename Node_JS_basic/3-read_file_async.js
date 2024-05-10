49854205ffc37f35a29e1923b1fe72a88139e23e
rivero04 <7710@holbertonstudents.com>
2024-05-10 05:43:11 -0700

    Task 3 and 4

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.promises.readFile(path, 'utf8')
+      .then((data) => {
+        const lines = data.split('\n');
+        const students = lines.filter((line, index) => line && index > 0);
+
+        let fields = {};
+
+        students.forEach(student => {
+          const info = student.split(',');
+          const field = info[3];
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(info[0]);
+        });
+
+        console.log(`Number of students: ${students.length}`);
+        for (const field in fields) {
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
+
+        resolve();
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
+  });
+}
+
+module.exports = countStudents;
