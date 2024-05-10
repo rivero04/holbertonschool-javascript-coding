f61ef2eb7795e5604c722eb849ae25245123250a
rivero04 <7710@holbertonstudents.com>
2024-05-10 05:31:49 -0700

    Task 1 and 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+function countStudents(path) {
+  let data;
+
+  try {
+    data = fs.readFileSync(path, "utf8");
+  } catch (err) {
+    throw new Error("Cannot load the database");
+  }
+
+  const lines = data.split("\n");
+  const students = lines.filter((line, index) => line && index > 0);
+
+  let fields = {};
+
+  students.forEach((student) => {
+    const info = student.split(",");
+    const field = info[3];
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(info[0]);
+  });
+
+  console.log(`Number of students: ${students.length}`);
+  for (const field in fields) {
+    console.log(
+      `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(", ")}`
+    );
+  }
+}
+
+module.exports = countStudents;
