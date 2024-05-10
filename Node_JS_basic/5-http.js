5c5f8f6e2ca4c2ac2141c2d44006e9c419810f60
rivero04 <7710@holbertonstudents.com>
2024-05-10 06:05:58 -0700

    fix again task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,13 +1,13 @@
-const http = require("http");
-const countStudents = require("./3-read_file_async");
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
-const databasePath = "database.csv";
+const databasePath = process.argv[2];
 
 const app = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  if (req.url === "/") {
-    res.end("Hello Holberton School!");
-  } else if (req.url === "/students") {
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
     countStudents(databasePath)
       .then((data) => {
         res.end(`This is the list of our students\n${data}`);
@@ -15,9 +15,9 @@ const app = http.createServer((req, res) => {
       .catch((error) => {
         res.end(`This is the list of our students\n${error.message}`);
       });
-  } 
+  }
 });
 
-app.listen(1245);
+app.listen(1245, '127.0.0.1')
 
 module.exports = app;
