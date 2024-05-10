284c426fbb46ec18ed18b84eefe0f83692f9b1ab
rivero04 <7710@holbertonstudents.com>
2024-05-10 06:11:14 -0700

    fix?

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,27 +1,20 @@
 const http = require('http');
-const fs = require('fs');
 const countStudents = require('./3-read_file_async');
 
-const databasePath = process.argv[2];
+const databasePath = 'database.csv';
 
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    fs.access(databasePath, fs.constants.F_OK, (err) => {
-      if (err) {
-        res.end('This is the list of our students\nDatabase not found');
-      } else {
-        countStudents(databasePath)
-          .then((data) => {
-            res.end(`This is the list of our students\n${data}`);
-          })
-          .catch((error) => {
-            res.end(`This is the list of our students\n${error.message}`);
-          });
-      }
-    });
+    countStudents(databasePath)
+      .then((data) => {
+        res.end(`This is the list of our students\n${data}`);
+      })
+      .catch((error) => {
+        res.end(`This is the list of our students\n${error.message}`);
+      });
   }
 });
 
