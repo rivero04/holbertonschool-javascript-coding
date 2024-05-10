15c168d2abd6812de038469d52e6a9554451a09c
rivero04 <7710@holbertonstudents.com>
2024-05-10 06:14:55 -0700

    task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const countStudents = require('./3-read_file_async');
 
 const databasePath = 'database.csv';
@@ -10,13 +11,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', (req, res) => {
-  countStudents(databasePath)
-    .then((data) => {
-      res.send(`This is the list of our students\n${data}`);
-    })
-    .catch((error) => {
-      res.send(`This is the list of our students\n${error.message}`);
-    });
+  fs.access(databasePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      res.send('This is the list of our students\nDatabase not found');
+    } else {
+      countStudents(databasePath)
+        .then((data) => {
+          res.send(`This is the list of our students\n${data}`);
+        })
+        .catch((error) => {
+          res.send(`This is the list of our students\n${error.message}`);
+        });
+    }
+  });
 });
 
 app.listen(1245);
